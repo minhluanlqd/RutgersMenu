7e1201e5aa80422653cc8221d58637e66ecb60d5
tan <nttrongtan1605@gmail.com>
2020-06-01 15:31:38 -0400

    navbar

---
+++ src/components/header/Header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "../../assets/images/rutgersLogo.png";
+import "./header.styles.scss";
+
+/*export const Header = () => (
+  <div className="header">
+    <img src={logo} alt='logo' />
+    <div className="options">Busch</div>
+    <div className="options">Livingston</div>
+    <div className="options">College Ave</div>
+    <div className="options">Cook/Douglass</div>
+  </div>
+);*/
+
+export const Header = () => (
+  <div>
+    <nav className="nav db dt-l w-100 border-box pa3 ph5-l">
+      <img
+        src={logo}
+        class="dib w2 h2 br-100"
+        alt="Site Name"
+      />
+
+      <a
+        className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
+        href="#"
+        title="Home"
+      >
+        Busch
+      </a>
+      <a
+        className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
+        href="#"
+        title="How it Works"
+      >
+        College Ave
+      </a>
+      <a
+        className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
+        href="#"
+        title="Blog"
+      >
+        Livingston
+      </a>
+      <a
+        className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
+        href="#"
+        title="Press"
+      >
+        Cook Douglass
+      </a>
+    </nav>
+  </div>
+);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import Card from '../components/Card'
-import './App.css';
+import React from "react";
+import Card from "../components/Card";
+import "./App.css";
+import { Header } from "../components/header/Header";
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Card></Card>
     </div>
   );
