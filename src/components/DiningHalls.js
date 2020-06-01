518e034e5d02d788b6becaf2097770a55303494f
Luan Tran <minhluanlqd@gmail.com>
2020-06-01 12:49:51 -0400

    first commit

---
+++ src/components/DiningHalls.js
@@ -0,0 +1,18 @@
+export const dininghalls = [
+    {
+        name: 'Busch Dining Hall',
+        address: '608 Bartholomew Rd, Piscataway, NJ 08854'
+    },
+    {
+        name: 'Livingston Dining Hall',
+        address: 'Joyce Kilmer Ave, Piscataway, NJ 08854'
+    },
+    {
+        name: 'College Avenue Dining Hall',
+        address: '145 College Ave, New Brunswick, NJ 08901'
+    },
+    {
+        name: 'Cook/Douglass Dining Hall',
+        address: 'New Brunswick, NJ 08901'
+    }
+];
