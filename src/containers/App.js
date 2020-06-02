300e9be4d6b9acc12cbabd3b2c43c9f34d9eff43
Luan Tran <minhluanlqd@gmail.com>
2020-06-02 08:31:15 -0400

    Minor changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Card from '../components/Card'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Card></Card>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    showMenu: false
+  };
+
+  toggleMenuHandler = () => {
+    const doesShow = this.state.showMenu;
+    this.setState({showMenu: !doesShow});
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Card></Card>
+      </div>
+    );
+  }
 }
 
 export default App;
