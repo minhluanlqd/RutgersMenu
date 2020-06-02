c28c825d31e5ad02a85fc2a290204a9077bf5f08
Luan Tran <minhluanlqd@gmail.com>
2020-06-02 08:36:33 -0400

    update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
-import Card from '../components/Card'
+import Card from '../components/Card';
+import { Header } from "../components/header/Header";
 import './App.css';
 
 class App extends Component{
@@ -17,25 +17,10 @@ class App extends Component{
   render(){
     return(
       <div className="App">
+        <Header/>
         <Card></Card>
       </div>
     );
   }
-=======
-import React from "react";
-import Card from "../components/Card";
-import "./App.css";
-import { Header } from "../components/header/Header";
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Card></Card>
-    </div>
-  );
->>>>>>> 0b7faba851e993dec5bc2e8260d743e5d75f99f3
 }
-
 export default App;
