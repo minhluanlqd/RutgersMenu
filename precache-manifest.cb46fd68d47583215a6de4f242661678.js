b571c18be3aa896037b40bcf7a8d1725414ac20d
Luan Tran <minhluanlqd@gmail.com>
2020-06-04 11:53:28 -0400

    Updates

---
+++ precache-manifest.cb46fd68d47583215a6de4f242661678.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932d975027dd8db297912c4cb2ac7244",
+    "url": "/RutgersMenu/index.html"
+  },
+  {
+    "revision": "91ae8a45db70f5790a40",
+    "url": "/RutgersMenu/static/css/main.13b2fbea.chunk.css"
+  },
+  {
+    "revision": "bcc4fefd8eb9da38ea59",
+    "url": "/RutgersMenu/static/js/2.6db64490.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/RutgersMenu/static/js/2.6db64490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ae8a45db70f5790a40",
+    "url": "/RutgersMenu/static/js/main.c41faa70.chunk.js"
+  },
+  {
+    "revision": "6543a516acfc157a9494",
+    "url": "/RutgersMenu/static/js/runtime-main.0845cdfd.js"
+  },
+  {
+    "revision": "5bfc0c36e6e9770150aebd91c0177cdd",
+    "url": "/RutgersMenu/static/media/chickensalad.5bfc0c36.jpg"
+  },
+  {
+    "revision": "272ec043440699cc66244143cc3fad50",
+    "url": "/RutgersMenu/static/media/chickentaco.272ec043.jpg"
+  },
+  {
+    "revision": "1b9fe474ae5f10052d4a4dd5a55066e0",
+    "url": "/RutgersMenu/static/media/cupcake.1b9fe474.jpg"
+  },
+  {
+    "revision": "b83a66f1a8c97ab0571985034e704416",
+    "url": "/RutgersMenu/static/media/dininghall-1.b83a66f1.jpg"
+  },
+  {
+    "revision": "b8dc2073312444b9b4fac8d415b44ffd",
+    "url": "/RutgersMenu/static/media/dininghall-2.b8dc2073.jpg"
+  },
+  {
+    "revision": "0875f300f6dfd53adfdf3681c58bbe83",
+    "url": "/RutgersMenu/static/media/dininghall-3.0875f300.jpg"
+  },
+  {
+    "revision": "9cc5153736abb6ea06adf30161a6e772",
+    "url": "/RutgersMenu/static/media/dininghall-4.9cc51537.jpg"
+  },
+  {
+    "revision": "ba330ddb2f40f3536f2875b418675a32",
+    "url": "/RutgersMenu/static/media/pizza.ba330ddb.jpg"
+  },
+  {
+    "revision": "ab38689610dcab592141ff20cbec525a",
+    "url": "/RutgersMenu/static/media/smashedpotato.ab386896.jpg"
+  }
+]);
