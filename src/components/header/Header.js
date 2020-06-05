75f3537ad65d5e9c4fa1b6810a439bfe482424f8
tan <nttrongtan1605@gmail.com>
2020-06-05 13:47:57 -0400

    fix navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,35 +5,51 @@ import "./header.styles.scss";
 export const Header = (props) => (
   <div>
     <nav className="nav db dt-l w-100 border-box pa3 ph5-l">
-      <img
-        src={logo}
-        className="dib w2 h2 br-100"
-        alt="Site Name"
-      />
+      <img src={logo} className="dib w2 h2 br-100" alt="Site Name" />
 
       <div
         className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
         href="#"
         title="Home"
-        onClick={props.clicked[0][1]}
+        onClick={() => {
+          props.clicked[0][1](true);
+          props.clicked[1][1](false);
+          props.clicked[2][1](false);
+          props.clicked[3][1](false);
+        }}
       >
         Busch
       </div>
       <div
         className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
-        onClick={props.clicked[1][1]}
+        onClick={() => {
+          props.clicked[0][1](false);
+          props.clicked[1][1](true);
+          props.clicked[2][1](false);
+          props.clicked[3][1](false);
+        }}
       >
         Livingston
       </div>
       <div
         className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
-        onClick={props.clicked[2][1]}
+        onClick={() => {
+          props.clicked[0][1](false);
+          props.clicked[1][1](false);
+          props.clicked[2][1](true);
+          props.clicked[3][1](false);
+        }}
       >
         College Ave
       </div>
       <div
         className="link dim dark-gray f6 f5-l dib mr3 mr4-l options"
-        onClick={props.clicked[3][1]}
+        onClick={() => {
+          props.clicked[0][1](false);
+          props.clicked[1][1](false);
+          props.clicked[2][1](false);
+          props.clicked[3][1](true);
+        }}
       >
         Cook Douglass
       </div>
